a361006216284cba549dd28aa70d32bc504a3fae
smallwinds <578057061@qq.com>
2018-07-03 09:38:54 +0800

    update script

--- CommonPackScript/MainBuild.bat
+++ CommonPackScript/MainBuild.bat
@@ -2,14 +2,11 @@
 
 REM 需要用户手动配置的内容
 
-REM 软件icon 目录
-SET ICON_PATH="D:\Pictures\icon.ico"
-
 REM 定义编译器根目录
 SET COMPILER="D:\Program Files\vs2015\VC\vcvarsall.bat"
 
 REM 定义目标路径
-SET TARTGET_DIR=D:\FutureStockTradingSystemClientRelease
+SET TARTGET_DIR=D:\PackageTest
 
 REM 定义临时路径
 SET TEMP_DIR=%TARTGET_DIR%\Temp
@@ -20,14 +17,14 @@ SET ARTIFACT_DIR=%TARTGET_DIR%\Artifact
 REM 工程解决方案目录
 SET SOLUTION_HOME=D:\FutureStockTradingSystemClient\FutureStockTradingSystemPcClient
 
+REM 工程入口文件
+SET SOLUTION_ENTRY_PATH=%SOLUTION_HOME%\FutureStockTradingSystemPcClient.sln
+
 REM PC端编译生成目录
 SET COMPILE_OUTPUT_DIR=%SOLUTION_HOME%\FutureStockTradingSystemPcClient\bin\Release
 
-
-
-del /S %SOLUTION_HOME%\FutureStockTradingSystemPcClient\icon.ico
-
-xcopy %ICON_PATH% %SOLUTION_HOME%\FutureStockTradingSystemPcClient
+REM nsis脚本路径
+SET NSIS_SCRIPT_PATH=.\pack_script.nsi
 
 
 REM 清理工程
@@ -46,13 +43,13 @@ MKDIR %ARTIFACT_DIR%
 
 REM 打包编译
 call %COMPILER%
-MSBuild %SOLUTION_HOME%\FutureStockTradingSystemPcClient.sln /t:Rebuild /P:WarningLevel=2;Configuration=release   
+MSBuild %SOLUTION_ENTRY_PATH% /t:Rebuild /P:WarningLevel=2;Configuration=release   
 
 REM 拷贝生成文件到临时目录
 xcopy /YE %COMPILE_OUTPUT_DIR% %TEMP_DIR%
 del /Q/S %TEMP_DIR%\*.pdb
 
 REM 打安装包
-makensis /DSorceFile_Path=%TEMP_DIR%\*.* /DTargetFile_Path=%ARTIFACT_DIR%\setup.exe ./FutureStockTradingSystemClient(withDotNet).nsi
+makensis /DSorceFile_Path=%TEMP_DIR%\*.* /DTargetFile_Path=%ARTIFACT_DIR%\setup.exe %NSIS_SCRIPT_PATH%
 
 PAUSE
